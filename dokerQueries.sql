90ca06689f24a3f39c33a6389db1404d002b92bf
IscPedroSanchezatos <pedro-francisco.sanchez@atos.net>
2022-07-21 10:46:54 -0500

    SQL queries

---
+++ dokerQueries.sql
@@ -0,0 +1,3 @@
+
+UPDATE film SET film.title = 'Thor: Love and Thunder', film.description= 'Sequel to "Thor: Ragnarok" in which the God of Thunder will have Lady Thor as his companion.', film.release_year= 2022, film.rental_rate = 3.99, film.`length`= 125, film.rating= 'PG-13'
+WHERE film.film_id=1;
